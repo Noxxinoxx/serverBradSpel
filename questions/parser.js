759f0a3840a910222729133f86b6ee3073d5264a
add33m <adam.gatmon@gmail.com>
2020-02-27 00:28:18 +0100

    Parser now works and saves to database

--- questions/parser.js
+++ questions/parser.js
@@ -1,27 +1,64 @@
 const fs = require('fs');
+const { performance } = require('perf_hooks');
 const mongoose = require('mongoose');
 
 // Connect to MongoDB Atlas DB using Mongoose
 const DBURL = "mongodb+srv://admin:X77xMVUWwSb7R7M@database-lqpxz.azure.mongodb.net/users?retryWrites=true&w=majority";
 mongoose.connect(DBURL, {useNewUrlParser: true})
   .then(() => {
-    console.log("Successfully connected to database!");
+    console.log("Successfully connected to database");
+    main();
   }, (err) => {
     console.log("Error while connecting to database! Err: " + err);
   });
 
 // Fetch Mongoose models
-const ConstQuestions = require('../mongoose-models/constquestion');
+const ConstQuestion = require('../mongoose-models/constquestion');
 
-const red = fs.readFileSync("./red.txt", {encoding: "UTF-8"});
-const redLines = red.split("\n");
+async function main() {
+  console.log("Running parser...");
+  const inTime = performance.now();
+  // Read files and split into lines
+  const red = fs.readFileSync("./red.txt", {encoding: "UTF-8"});
+  const redLines = red.split("\n");
 
-for (str of redLines) {
-  const questionPattern = /(\w.+?) =/;
-  const question = questionPattern.exec(str)[1];
-  const corrAnswerPattern = /\((.+?)\)/;
-  const corrAnswer = corrAnswerPattern.exec(str)[1];
-  const incoAnswerPattern = /(\)|\.) [\w-,° ]+\./g;
-  const incoAnswers = incoAnswerPattern.exec(str)[1];
-  console.log(`Fråga: ${question} Svar: ${corrAnswer} Fel svar: ${JSON.stringify(incoAnswers)}`);
+  // Delete all entries in database so that no duplicates will exist
+  await ConstQuestion.deleteMany({}, err => {
+    console.log(err || "Successfully deleted all entries from database");
+  })
+
+  let questionCount = 0;
+
+  // Loop over red questions and find all parts using RegExp (urghh)
+  for (str of redLines) {
+    const questionPattern = /(\w.+?) =/;
+    const question = questionPattern.exec(str)[1];
+    const corrAnswerPattern = /\((.+?)\)/;
+    const corrAnswer = corrAnswerPattern.exec(str)[1];
+    const incoAnswerPattern = /\. [\wåäö°, \-\+]+|\) [\wåäö°, \-\+]+/gi;
+    const incoAnswers = str.match(incoAnswerPattern);
+    const incoAns1 = incoAnswers[0].substring(2);
+    const incoAns2 = incoAnswers[1].substring(2);
+    const incoAns3 = incoAnswers[2].substring(2);
+    // console.log(`Fråga: ${question}, Svar: ${corrAnswer}, Fel svar 1: ${incoAns1}, Fel svar 2: ${incoAns2}, Fel svar 3: ${incoAns3}`);
+
+    const newQuestion = new ConstQuestion({
+      title: question,
+      a1: { a: corrAnswer, c: true },
+      a2: { a: incoAns1, c: false },
+      a3: { a: incoAns2, c: false },
+      a4: { a: incoAns3, c: false },
+      categ: "Övrigt",
+      diff: { e: false, m: false, h: true },
+    });
+    await newQuestion.save(err => {
+      if (err)
+        console.log(err);
+    });
+    questionCount++;
+  }
+  console.log(`Finished adding all (${questionCount}) entries to database`)
+
+  const endTime = performance.now()
+  console.log(`Parser finished! (${Math.floor(endTime - inTime + .5)}ms)`);
 }
